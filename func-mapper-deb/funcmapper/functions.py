7cd2dbd5cd0c07c3cca716ae54a35eb052425917
Muzammil Abdul Rehman <muzammil.abdul.rehman@gmail.com>
2023-04-04 14:23:51 +0500

    [FMP] Basic Function Mapper Example

---
+++ func-mapper-deb/funcmapper/functions.py
@@ -0,0 +1,11 @@
+"""A list of functions to be mapped"""
+
+
+def rails(name: str, length: str) -> str:
+    pass
+
+def cylinders(name: str, length: str, diamter: str = None) -> str:
+    pass
+
+def oranges(**kwargs):
+    pass
